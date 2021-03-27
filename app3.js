3588292ec53df834488eefc2c89dd998581b7173
SolRocca <solcito2502@gmail.com>
2021-03-26 21:34:19 -0300

    [FIX] app3.js

--- app3.js
+++ app3.js
@@ -1,3 +1,7 @@
 console.log(8+8)
 
 Y acá da 16
+
+Meli me cuenta sobre ella y yo le cuento sobre mí, mientras editamos el archivo.
+Es viernes a la noche y tenemos más ganas de una birra que de hacer esto.
+Tu cuenta da 4.
